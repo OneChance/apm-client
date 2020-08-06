49628813e85af6f23c3d0c048d538acb88865c31
onechance <zhouh08@hotmail.com>
2020-08-06 11:26:00 +0800

    改造前备份

---
+++ app/components/MissionAlloc.vue
@@ -0,0 +1,26 @@
+<template>
+    <audit-component v-bind:step="'missionAlloc'"></audit-component>
+</template>
+
+<script>
+
+import AuditComponent from "./AuditComponent";
+
+export default {
+    name: "MissionAlloc",
+    created: function () {
+
+    },
+    mounted() {
+    },
+    data: function () {
+        return {}
+    },
+    methods: {},
+    components: {AuditComponent}
+}
+</script>
+
+<style scoped>
+
+</style>
